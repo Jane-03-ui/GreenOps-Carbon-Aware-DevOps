9fd22304a02c8c18ec0300036d4ca09d7bdf86d6
Jane-03-ui <jananiijane03@gmail.com>
2026-03-16 17:15:38 +0530

    Update carbon_api.py

--- engine/carbon_api.py
+++ engine/carbon_api.py
@@ -1,1 +1,31 @@
+import json
+import random
+from datetime import datetime
 
+ZONES = [
+    "IN-SO",
+    "SG",
+    "US-VA",
+    "US-OR",
+    "DE",
+    "IE",
+    "JP"
+]
+
+def generate_carbon_data():
+
+    results = {}
+
+    for zone in ZONES:
+        results[zone] = {
+            "carbonIntensity": random.randint(80, 600),
+            "datetime": str(datetime.now())
+        }
+
+    with open("data/carbon_data.json", "w") as file:
+        json.dump(results, file, indent=4)
+
+    print("Simulated carbon data generated!")
+
+if __name__ == "__main__":
+    generate_carbon_data()
