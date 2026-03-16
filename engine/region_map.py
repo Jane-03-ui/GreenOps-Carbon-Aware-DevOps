dc0e4ecaaa7718968e35e78816207bb79cef4abe
Jane-03-ui <jananiijane03@gmail.com>
2026-03-16 14:36:56 +0530

    Create region_map.py
    Create region mapping module

---
+++ engine/region_map.py
@@ -0,0 +1,10 @@
+# Mapping AWS regions to electricity zones
+region_map = {
+    "ap-south-1": "IN-SO",
+    "ap-southeast-1": "SG",
+    "us-east-1": "US-VA",
+    "us-west-2": "US-OR",
+    "eu-west-1": "IE",
+    "eu-central-1": "DE",
+    "ap-northeast-1": "JP"
+}
