e626c2239d24f678d535c252ba235f375c51d05d
Jane-03-ui <jananiijane03@gmail.com>
2026-03-16 14:44:12 +0530

    Update region_map.py

--- engine/region_map.py
+++ engine/region_map.py
@@ -1,10 +1,17 @@
-# Mapping AWS regions to electricity zones
+# Map AWS EC2 regions to Electricity Maps zones
+
 region_map = {
-    "ap-south-1": "IN-SO",
-    "ap-southeast-1": "SG",
-    "us-east-1": "US-VA",
-    "us-west-2": "US-OR",
-    "eu-west-1": "IE",
-    "eu-central-1": "DE",
-    "ap-northeast-1": "JP"
+    "ap-south-1": "IN-SO",        # Mumbai
+    "ap-southeast-1": "SG",       # Singapore
+    "us-east-1": "US-VA",         # Virginia
+    "us-west-2": "US-OR",         # Oregon
+    "eu-west-1": "IE",            # Ireland
+    "eu-central-1": "DE",         # Frankfurt
+    "ap-northeast-1": "JP"        # Tokyo
 }
+
+def get_regions():
+    return list(region_map.keys())
+
+def get_zone(region):
+    return region_map.get(region)
